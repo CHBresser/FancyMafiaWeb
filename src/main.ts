fdc767ed430730919b94cdf0380f743fdc21b67a
Zach Bresser <Z@chbresser.com>
2019-12-05 20:48:04 +0000

    Init fwb

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import BootstrapVue from "bootstrap-vue";
 
+// polyfills for older IE, android, and IOS web browsers
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import "intersection-observer";
+
+// global scss
+import "./App.scss";
+
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
